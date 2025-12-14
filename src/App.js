407606e1784e5c0dc16fd49fe35902029141be82
developersahaj-cmd <developer.sahaj@gmail.com>
2025-12-14 14:36:03 +0530

    src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GitGrade from './GitGrade';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GitGrade />
+    </div>
+  );
+}
+
+export default App;
